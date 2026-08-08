45ffc2d59c4b1b04212b2a5707f8b80ef3c1c899
LAG Dev <dev@lag-app.local>
2026-08-08 11:11:08 +0200

    fix github actions link

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === "development" ? "/" : "/lag_app/",
-  plugins: [react()],
-  server: {
-    port: 5173,
-  },
-}));
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    base: env.VITE_BASE_PATH || (mode === "development" ? "/" : "/lag_app/"),
+    plugins: [react()],
+    server: {
+      port: 5173,
+    },
+  };
+});
